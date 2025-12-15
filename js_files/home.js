88c4280750f45a96f1040e42646727225c6f1d1d
soultabz <tabo.khumalo26@gmail.com>
2025-12-15 23:33:36 +0200

    add more button functionality and dom display

--- js_files/home.js
+++ js_files/home.js
@@ -1,4 +1,4 @@
-const createBtn = document.querySelector(".create-btn")
+const createBtn = document.querySelector(".create-btn");
 const createForm = document.querySelector("form");
 const saveBtn = document.querySelector("#save");
 const blurFilter = document.querySelector(".overlay");
@@ -9,6 +9,8 @@ const pagesInput = document.querySelector("#pages");
 const readInput = document.querySelector("#read");
 const notReadInput = document.querySelector("#not-yet");
 const bookForm = document.querySelector("#bookForm");
+const cardContainer = document.querySelector(".card-container");
+let myLibrary = [];
 
 
 // Functions
@@ -33,12 +35,76 @@ function clearForm() {
   bookForm.reset();
 }
 
+function addBookToLibrary(title, author, pages, read) {
+  const newBook = new Book(title, author, pages, read);
+  myLibrary.push(newBook);
+  displayBooks();
+}
 
+function removeBook(id) {
+    myLibrary = myLibrary.filter(book => book.id !== id);
+    displayBooks();
+}
+
+function displayBooks() {
+  myLibrary.forEach((book) => {
+
+    // card container
+    const card = document.createElement("div")
+    card.classList.add("card");
+
+    // title div and title
+    const titleContainer = document.createElement("div");
+    titleContainer.classList.add("title");
+
+    const title = document.createElement("h4");
+    title.textContent = book.title;
+
+    card.appendChild(titleContainer);
+    titleContainer.append(title);
+
+    // author div and author
+    const authorContainer = document.createElement("div");
+    authorContainer.classList.add("author");
+
+    const author = document.createElement("p")
+    author.textContent = book.author;
+
+    card.appendChild(authorContainer);
+    authorContainer.appendChild(author);
+
+    // pages div and pages
+
+    const pagesContainer = document.createElement("div");
+    pagesContainer.classList.add("pages");
+
+    const pages = document.createElement("p")
+    pages.textContent = book.pages;
+
+    card.appendChild(pagesContainer);
+    pagesContainer.appendChild(pages)
+
+    // remove btn container and remove btn
+
+    const removeBtnContainer = document.createElement("button");
+    removeBtnContainer.classList.add("removeBtnCont");
+
+    const removeBtn = document.createElement("button")
+    removeBtn.innerText = "remove";
+    removeBtn.classList.add("removeBtn");
+
+    card.appendChild(removeBtnContainer);
+    removeBtnContainer.appendChild(removeBtn)
+
+    removeBtn.addEventListener('click', removeBook(book.id));
+
+    // card container
+
+    cardContainer.appendChild(card);
+  })
+};
 
 // Event Listeners
-
-
-
 // button to cancel and close form
 cancelBtn.addEventListener('click', (e) => {
   addClass(createForm, "hidden");
@@ -56,12 +122,28 @@ createBtn.addEventListener('click', (e) => {
 saveBtn.addEventListener('click', (e) => {
   addClass(createForm, "hidden");
   removeBlur(blurFilter, "blur-overlay");
+  addBookToLibrary(title.value, author.value, pages.value);
+  console.log(title.value);
+  clearForm();
 })
 
 // prevents form from reloading on submit
 bookForm.addEventListener("submit", (event) => {
   event.preventDefault();
 })
+
+
+// Construtor Function
+
+function Book(title, author, pages, read){
+  this.title = title,
+  this.author = author,
+  this.pages = pages,
+  this.read = read,
+  this.id = crypto.randomUUID();
+}
+
+displayBooks()
 
 
 
